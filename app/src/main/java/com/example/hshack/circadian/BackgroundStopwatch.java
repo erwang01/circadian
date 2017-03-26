738b91102fd115367a1f7c0228bbd56ee0a03a9f
HsHack <hshack@e1z1r6p42.42.us.org>
2017-03-25 23:34:05 -0700

    added sleep times

--- app/src/main/java/com/example/hshack/circadian/BackgroundStopwatch.java
+++ app/src/main/java/com/example/hshack/circadian/BackgroundStopwatch.java
@@ -3,14 +3,19 @@ package com.example.hshack.circadian;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
+import android.os.SystemClock;
+
 
 public class BackgroundStopwatch extends Service {
     public Context context = this;
     public Handler handler = null;
     public static Runnable runnable = null;
+    private long initial_time = 0;
+    long timeInMilliseconds = 0L;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -19,6 +24,7 @@ public class BackgroundStopwatch extends Service {
 
     @Override
     public void onCreate() {
+        SQLiteDatabase mydatabase = openOrCreateDatabase()
         Toast.makeText(this, "Service created!", Toast.LENGTH_LONG).show();
 
         handler = new Handler();
@@ -36,11 +42,14 @@ public class BackgroundStopwatch extends Service {
     @Override
     public void onDestroy() {
         handler.removeCallbacks(runnable);
+        timeInMilliseconds = SystemClock.elapsedRealtime() - initial_time;
+
         Toast.makeText(this, "Service stopped", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) { 
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        initial_time = SystemClock.elapsedRealtime();
         return START_STICKY;
     }
 }
---
+++ app/src/main/java/com/example/hshack/circadian/SleepDbHelper.java
@@ -0,0 +1,10 @@
+package com.example.hshack.circadian;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by hshack on 3/25/17.
+ */
+
+public class SleepDbHelper extends SQLiteOpenHelper {
+}
