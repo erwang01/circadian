2abfba016e4b38dc09c00f870cbd01126a1fd2b8
HsHack <hshack@e1z1r6p40.42.us.org>
2017-03-26 00:15:49 -0700

    Add notification support

--- app/src/main/java/com/example/hshack/circadian/BackgroundStopwatch.java
+++ app/src/main/java/com/example/hshack/circadian/BackgroundStopwatch.java
@@ -1,16 +1,23 @@
 package com.example.hshack.circadian;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.Toast;
 
 public class BackgroundStopwatch extends Service {
     public Context context = this;
     public Handler handler = null;
     public static Runnable runnable = null;
+    public NotificationCompat.Builder mBuilder;
+    public NotificationManager mNotificationManager;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -19,7 +26,20 @@ public class BackgroundStopwatch extends Service {
 
     @Override
     public void onCreate() {
-        Toast.makeText(this, "Service created!", Toast.LENGTH_LONG).show();
+        mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("Circadian")
+                .setContentText("Sleep timer started.");
+        //Intent resultIntent = new Intent(this, BackgroundStopwatch.class);
+       // TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        //stackBuilder.addParentStack(BackgroundStopwatch.class);
+        //stackBuilder.addNextIntent(resultIntent);
+        mBuilder.setOngoing(true);
+        //PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+       // mBuilder.setContentIntent(resultPendingIntent);
+        mNotificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
 
         handler = new Handler();
         runnable = new Runnable() {
@@ -36,11 +56,12 @@ public class BackgroundStopwatch extends Service {
     @Override
     public void onDestroy() {
         handler.removeCallbacks(runnable);
+        mNotificationManager.cancel(1);
         Toast.makeText(this, "Service stopped", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) { 
+    public int onStartCommand(Intent intent, int flags, int startId) {
         return START_STICKY;
     }
 }
